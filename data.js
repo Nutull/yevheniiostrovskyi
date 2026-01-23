6c15c75fe05885bdacee49be4c0fde240406c554
Nutull <62216428+Nutull@users.noreply.github.com>
2026-01-23 20:48:13 +0200

    Update projectsData with new project entries
    Replaced old project data with new entries for Film Camera, Arc Reactor, and Demogorgon, including detailed descriptions and updated galleries.

--- data.js
+++ data.js
@@ -1,31 +1,23 @@
 const projectsData = [
     {
-        id: "pupupu",
-        title: "PUPUPU",
-        image: "img/test.jpg",
-        description: "its a demo",
-        gallery: ["img/test.jpg", "img/test1.jpg", "img/test2.jpg"]
+        id: "fotik",
+        title: "Film Camera",
+        image: "img/fotik/MAIN.jpg",
+        description: `This project is a deep dive into creating a photorealistic asset with a primary focus on geometric precision and micro-detailing. My goal was to build a model capable of withstanding extreme macro shots while maintaining physical accuracy under any lighting conditions.`,
+        gallery: ["img/fotik/MAIN.jpg", "img/fotik/x3_4K.jpg","img/fotik/front.jpg","img/fotik/back.jpg","img/fotik/detail_1.jpg","img/fotik/detail_2.jpg","img/fotik/detail_3.jpg","img/fotik/detail_4.jpg","img/fotik/detail_5.jpg","img/fotik/white.jpg", "img/fotik/wire_05.jpg"]
     },
     {
-        id: "pepe",
-        title: "PEPE",
-        image: "img/images (1).jpg",
-        description: "i told you its a demo cmon",
-        gallery: ["img/images (1).jpg"]
+        id: "arc",
+        title: "Arc Reactor",
+        image: "img/arc/prev.jpeg",
+        description: "This project focuses on recreating the iconic piece of technology with an emphasis on intricate mechanical assembly and cinematic lighting effects. The goal was to achieve a high level of realism through complex layering of materials and light sources.",
+        gallery: ["img/arc/prev.jpeg", "img/arc/edit.mp4", "img/arc/2.jpeg", "img/arc/norm.jpeg", "img/arc/pre.mp4", "img/arc/setka.jpeg"]
     },
     {
-        id: "vatafa",
-        title: "VATAFA",
-        image: "img/images (2).jpg",
-        description: "there is no words",
-        gallery: ["img/images (1).jpg"]
-    },
-    {
-        id: "tuntuntun",
-        title: "TUNTUNTUN",
-        image: "img/images (1).jpg",
-        description: "3.1415926535",
-        gallery: ["img/images (1).jpg", "img/photo_2026-01-17_15-22-00.jpg"]
+        id: "demo",
+        title: "Demogorgon",
+        image: "img/demo/TEST_1_edited.jpg",
+        description: "This project explores the adaptation of a complex, organic creature into a simplified, stylized toy aesthetic. The challenge was to maintain the recognizable, terrifying features of the Demogorgon while conforming to the iconic Funko Pop design language.",
+        gallery: ["img/demo/TEST_1_edited.jpg",  "img/demo/grey_4K.jpg", "img/demo/back.jpg", "img/demo/cir.mp4", "img/demo/grey_4K._detail_1.jpg", "img/demo/grey_4K._detail_2.jpg", "img/demo/back_detail.jpg"]
     }
-
 ];
