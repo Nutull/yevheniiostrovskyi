1526574648ba86cab63e2a5be1b80590335482e6
Nutull <62216428+Nutull@users.noreply.github.com>
2026-01-25 11:02:11 +0200

    Add 'Karabiner 98k' project to projectsData
    Added a new project entry for 'Karabiner 98k' with detailed description and gallery images.

--- data.js
+++ data.js
@@ -19,6 +19,14 @@ const projectsData = [
         image: "img/demo/TEST_1_edited.jpg",
         description: "This project explores the adaptation of a complex, organic creature into a simplified, stylized toy aesthetic. The challenge was to maintain the recognizable, terrifying features of the Demogorgon while conforming to the iconic Funko Pop design language.",
         gallery: ["img/demo/TEST_1_edited.jpg",  "img/demo/grey_4K.jpg", "img/demo/back.jpg", "img/demo/cir.mp4", "img/demo/grey_4K._detail_1.jpg", "img/demo/grey_4K._detail_2.jpg", "img/demo/back_detail.jpg"]
+    },
+    {
+        id: "kar98",
+        title: "Karabiner 98k",
+        image: "img/kar98/scene_1.jpg",
+        description: "This is a Mauser Kar98k modeling project focused on geometric accuracy and realistic textures. I created PBR materials to show the natural wear on the wood and metal surfaces. The final renders use atmospheric lighting to present the rifle in a workshop setting.",
+        gallery: ["img/kar98/scene_1.jpg", "img/kar98/front.jpg", "img/kar98/back.jpg", "img/kar98/detail.jpg", "img/kar98/detail_1.jpg", "img/kar98/detail_2.jpg", "img/kar98/detail_3.jpg", "img/kar98/0000-0240.mp4", "img/kar98/edit.jpg", "img/kar98/scene.jpg", "img/kar98/white.jpg"]
     }
 ];
+
 
