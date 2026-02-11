51e5dbcddd6bc9aa16a3a11eb4334cadfd80dc7b
Nutull <62216428+Nutull@users.noreply.github.com>
2026-02-11 23:01:28 +0200

    Refactor projectsData with updated project entries
    Updated projectsData array with new project details and descriptions.

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ const projectsData = [
         image: "img/fnaf_post/top.jpg",
         description: `I’m excited to share the result of my long and hard work—a short 3D animated film based on the Five Nights at Freddy’s universe.
 Project Highlights:
-- handmade assets: Every model, environment, and texture was created manually by me.
+- handmade assets: every model, environment, and texture was created manually by me.
 - atmosphere and sound: I put a lot of effort into the sound design and final compositing. I tried to capture that signature, oppressive atmosphere of the original through lighting and ambient effects.
 - i’ll be honest - the animation is a bit rough in places. I’m not a professional animator yet.
 
@@ -69,6 +69,7 @@ Also below you can see some "behind the scenes" shots.`,
         gallery: ["img/room_opt/main_def.jpg", "img/room_opt/main.jpg", "img/room_opt/detail_1.jpg", "img/room_opt/detail_2.jpg", "img/room_opt/detail_3.jpg", "img/room_opt/white.jpg"]
     }
 ];
+
 
 
 
